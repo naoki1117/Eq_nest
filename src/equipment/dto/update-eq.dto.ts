690fdbbc0c6d179da3208a5bd1052f0330726f9f
Your Name <you@example.com>
2023-03-26 07:12:52 +0900

    deploy2

--- src/equipment/dto/update-eq.dto.ts
+++ src/equipment/dto/update-eq.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsOptional, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
 
 export class updateEq {
   @IsString()
