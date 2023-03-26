1baab4df2356b6b54957528029c99721c51ab24d
Your Name <you@example.com>
2023-03-26 09:23:07 +0900

    hethh46j

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://eq-next.vercel.app'],
   });
   app.use(CookieParser());
   app.use(
